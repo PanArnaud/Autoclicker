34c0a638e2d26ed256b7336a3269365fb60f6509
a.panapadeatchy <a.panapadeatchy@rt-iut.re>
2015-12-18 18:20:56 +0400

    Loop works fine.
    Now, I have to add a windows to allow user to change the delay between
    two clicks, and how many clicks he want. I have to find a way to start
    clicks by pressure on space bar(or F10)

--- src/Autocliker.java
+++ src/Autocliker.java
@@ -1,11 +1,32 @@
+import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.MouseEvent;
 
 public class Autocliker {
-	private static Robot robot = nulls;
+	private static Robot robot = null;
 
 	public static void main(String[] args) {
+		try{
+			robot = new Robot();
+		} catch(AWTException e){
+			e.printStackTrace();
+		}
 		
-
+		int i = 1;  
+		
+		while(i != 100){
+			click(800, 450);
+			i++;
+		}
+			
+		
+		
 	}
 
+	public static void click(int x, int y){
+		robot.mouseMove(x,  y);
+		robot.delay(25);
+		robot.mousePress(MouseEvent.BUTTON1_MASK);
+		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
+	}
 }
