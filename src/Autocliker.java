b1a81b74d70031b3db5b482d1f4eb07864967b4c
a.panapadeatchy <a.panapadeatchy@rt-iut.re>
2015-12-22 18:57:54 +0400

    Add InputDialog
    Version 1.0 release

--- src/Autocliker.java
+++ src/Autocliker.java
@@ -1,9 +1,21 @@
+/*
+	Author: Arnaud Panapadéatchy
+	GitHub: ArnaudPan
+	Website: http://ar-pa.esy.es
+	Version: 1.0
+*/
+
 import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
+import javax.swing.JOptionPane;
 
 public class Autocliker {
 	private static Robot robot = null;
+	private static int time;
 
 	public static void main(String[] args) {
 		try{
@@ -12,17 +24,28 @@ public class Autocliker {
 			e.printStackTrace();
 		}
 		
-		int i = 1;  
+		try {
+			time =  Integer.parseInt(JOptionPane.showInputDialog("Temps: "));
+		} catch (Exception e) {
+			System.exit(0);
+			}
 		
-		while(i != 100){
-			click(800, 450);
+		Point coordinates = getCoordinates();
+		
+		int i = 1;  
+		while(i != (1000 / 25) * time){
+			click(coordinates.x, coordinates.y);
 			i++;
 		}
-			
-		
-		
+		System.exit(0);
 	}
-
+	
+	public static Point getCoordinates(){
+		PointerInfo pointer = MouseInfo.getPointerInfo();
+		Point location = pointer.getLocation();
+		return location;
+	}
+	
 	public static void click(int x, int y){
 		robot.mouseMove(x,  y);
 		robot.delay(25);
